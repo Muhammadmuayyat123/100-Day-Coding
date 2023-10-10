29cad08138473fb9e24f6a522d4c2d1205b329e1
yayat <muhiyat9@gmail.com>
2023-10-10 16:57:16 +0700

    Day One coy

---
+++ Day001.java
@@ -0,0 +1,25 @@
+
+public class Day001 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String nama;
+        String domisili, fakultas;
+        int angkatan;
+        double jarak;
+
+        nama = "muhammad muayyat sabir";
+        domisili = "majene";
+        fakultas = "Teknik";
+        angkatan = 2021;
+        jarak = 2.9;
+
+        System.out.println("Halo perkenalkan nama saya " + nama + " saya asli " + domisili);
+        System.out.println("saya angkatan " + angkatan + " dari fakultas " + fakultas);
+        System.out.println("jarak kampus ke rumah saya " + jarak + " km");
+
+
+
+    }
+}
