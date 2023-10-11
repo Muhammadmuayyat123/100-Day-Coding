28910b4a0510f369327421cfff17464f9ffb4812
yayat <muhiyat9@gmail.com>
2023-10-11 22:06:41 +0700

    Day tree boyss

--- Day003.java
+++ Day003.java
@@ -1,5 +1,16 @@
 public class Day003 {
     public static void main(String[] args) {
-        
+        int sisi, luas;
+        int alas, tinggi;
+        double hasil;
+
+        sisi = 12;
+        luas = sisi * sisi;
+
+        alas = 12;
+        tinggi = 30;
+        hasil = 0.5 * alas * tinggi;
+        System.out.println("Luas persegi : " + luas);
+        System.out.println("Luas segitiga : " + hasil);
     }
 }
