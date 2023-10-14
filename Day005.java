92fda065b51d91476eb859da194d7b6a59e33287
yayat <muhiyat9@gmail.com>
2023-10-14 18:16:04 +0700

    Day five? legooo

---
+++ Day005.java
@@ -0,0 +1,17 @@
+public class Day005 {
+
+    public static void main(String[] args) {
+        String nama_pertama;
+        String nama_tengah;
+        String nama_akhir;
+        
+        nama_pertama = "muhammad";
+        nama_tengah = "muayyat";
+        nama_akhir = "sabir";
+        
+        String nama_lengkap = nama_pertama + " " + nama_tengah + " " + nama_akhir;
+
+        // Menampilkan hasil penggabungan string
+        System.out.println("Nama Lengkap: " + nama_lengkap);
+    }
+}
