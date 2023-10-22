dc61812636d20d45973ddb9745d68f2df50751a7
yayat <muhiyat9@gmail.com>
2023-10-22 19:31:56 +0700

    Day eleven tipe data Non primitif

---
+++ Day011.java
@@ -0,0 +1,20 @@
+public class Day011 {
+    public static void main(String[] args) {
+        
+        String[] daftarMahasiswa = new String[5];
+
+        daftarMahasiswa[0] = "Cicci";
+        daftarMahasiswa[1] = "Kaco";
+        daftarMahasiswa[2] = "Cuali";
+        daftarMahasiswa[3] = "Manto";
+        daftarMahasiswa[4] = "Kecul";
+
+        System.out.println("Daftar Mahasiswa:");
+        System.out.println("1. " + daftarMahasiswa[0]);
+        System.out.println("2. " + daftarMahasiswa[1]);
+        System.out.println("3. " + daftarMahasiswa[2]);
+        System.out.println("4. " + daftarMahasiswa[3]);
+        System.out.println("5. " + daftarMahasiswa[4]);
+
+    }
+}
