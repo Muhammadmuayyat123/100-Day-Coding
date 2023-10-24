4f49814ad8f60a011d4c155f78cee8a60d861b6e
yayat <muhiyat9@gmail.com>
2023-10-24 19:37:09 +0700

    Day twelve Tipe data non primitive

---
+++ Day012.java
@@ -0,0 +1,15 @@
+public class Day012 {
+    public static void main(String[] args) {
+        String teks = "Contoh String";
+        Integer nilaiInteger = Integer.MAX_VALUE;
+        Long nilaiLong = Long.MAX_VALUE;
+        Double nilaiDouble = Double.MAX_VALUE;
+        Character karakter = 'A';
+
+        System.out.println("Tidak ada batasan.");
+        System.out.println("Tipe data Integer: Nilai maksimal = " + nilaiInteger);
+        System.out.println("Tipe data Long: Nilai maksimal = " + nilaiLong);
+        System.out.println("Tipe data Double: Nilai maksimal = " + nilaiDouble);
+        System.out.println("Tidak ada batasan");
+    }
+}
