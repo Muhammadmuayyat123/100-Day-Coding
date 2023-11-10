8bb54c9f0e464dd1a35e7761269a19da4d8744d2
yayat <muhiyat9@gmail.com>
2023-11-10 07:54:24 +0700

    while-bilangan genap

---
+++ Day025.java
@@ -0,0 +1,11 @@
+public class Day025 {
+    public static void main(String[] args) {
+        Integer i = 1;
+        System.out.println("Bilangan ganjil : ");
+        while (i <= 20) {
+            System.out.println(i);
+            i += 2;
+        }
+
+    }
+}
