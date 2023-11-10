f0cc2669af1aeda26b1e5cd589be2fc6d4dfadde
yayat <muhiyat9@gmail.com>
2023-11-10 21:36:56 +0700

    perulangan

---
+++ Day026.py
@@ -0,0 +1,13 @@
+nilai = int(input("Masukkan nilai pertama : "))
+
+tampung = []
+
+for i in range(nilai):
+    nilai2 = int(input(f"masukkan nilai {i + 1}: "))
+    
+    tampung.append(nilai2)
+    
+print("index terakhir : ", tampung[-1])
+tampung.pop(0)
+print(tampung)
+    
