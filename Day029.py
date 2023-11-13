7ebcc0ec8e961e3c82b3d70e294f3abe236ef551
yayat <muhiyat9@gmail.com>
2023-11-13 18:32:41 +0700

    latihan UK

--- Day029.py
+++ Day029.py
@@ -1,37 +1,3 @@
-# batas = int(input("Masukkan batas array: "))
-# genap = 0
-# ganjil = 0
-
-# for i in range(1, batas+1):
-#     if i % 2 == 0:
-#         genap += i
-#     else:
-#         ganjil += i
-# print("perjumlahan array bilangan genap = ", genap)
-# print("perjumlahan array bilangan ganjil = ", ganjil)
-
-
-
-
-
-# batas = int(input("masukkan batas : "))
-
-# def genap():
-#     for i in range(1, batas+1):
-#         if i % 2 == 0:
-#             nilai_genap += 1
-            
-
-# def ganjil():
-#     for i in range(1, batas+1):
-#         if i % 2 != 0:
-#             nilai_ganjil += 1
-
-# print(genap(nilai_genap))
-# print(ganjil(nilai_ganjil))
-
-
-
 def main():
     batas = int(input("Masukkan nilai : "))
     genap = 0
