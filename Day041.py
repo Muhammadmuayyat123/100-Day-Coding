52dbf48559b9b9c3ef82e889ddace8f3f3833127
yayat <muhiyat9@gmail.com>
2023-11-21 09:11:02 +0700

    Latihan UK

--- Day041.py
+++ Day041.py
@@ -1,16 +1,7 @@
-# apel_kg = 5000
-# pisang_kg = 8000
-# anggur_kg = 12000
+harga_apel = 5000  
+harga_pisang = 8000  
+harga_anggur = 12000  
 
-# total = apel_kg**2 + pisang_kg + anggur_kg/100
-
-# print(total)
-
-harga_apel = 5000  # rupiah
-harga_pisang = 8000  # rupiah
-harga_anggur = 12000  # rupiah
-
-# Jumlah yang dibeli oleh Ibu
 jumlah_apel = 2  
 jumlah_pisang = 1  
 jumlah_anggur = 0.5  
---
+++ Day042.py
@@ -0,0 +1,6 @@
+panjang_kolam = 20
+lebar_kolam = 10
+
+keliling_kolam  = 2 * (panjang_kolam + lebar_kolam)
+
+print(f"total panjang garis mengelilingi kolam {keliling_kolam} meter")
