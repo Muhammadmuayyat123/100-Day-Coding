2fc59357a1ac1fcf9fae4fb161ccf09453802ac8
yayat <muhiyat9@gmail.com>
2024-01-15 15:40:36 +0700

    variable dart

---
+++ Day059.py
@@ -0,0 +1,9 @@
+nilai1 = int(input(": "))
+nilai2 = int(input(": "))
+
+hasil = nilai1 + nilai2
+
+if hasil % 2 == 0:
+    hasil += 1
+else:
+    hasil += 2
---
+++ Day068.dart
@@ -0,0 +1,9 @@
+void main(){  
+  var judul = "Muhammad muayyat sabir";
+  String keterangan = "Melakukan KPI di makassar";
+  int tinggi = 170;
+  double beratBadan = 106.0;
+  print("Saya bernama $judul dan saya sedang $keterangan");
+  print("tinggi saya = $tinggi");
+  print("dengan berat badan = $beratBadan");
+}
