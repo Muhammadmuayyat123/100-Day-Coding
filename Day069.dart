6ad66c9c67e0915b0bd887c8f16b6358b6ec0c09
yayat <muhiyat9@gmail.com>
2024-01-15 15:47:03 +0700

    variabel dart

---
+++ Day069.dart
@@ -0,0 +1,12 @@
+void main(){
+    String nama = "Muhammad muayyat sabir";
+    int umur = 21;
+    double tinggi = 170.0;
+    bool isMenikah = false;
+
+    var alamat = "Majene, Sulawesi barat";
+
+    print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+    print("Menikah: $isMenikah");
+    print("Alamat: $alamat");
+}
