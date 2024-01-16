e5cc6169c870abd99dd3241b79351bcfd8d636cf
yayat <muhiyat9@gmail.com>
2024-01-16 13:37:58 +0700

    perbandingan

--- Day072.dart
+++ Day072.dart
@@ -1,1 +1,17 @@
+void main() {
+  int ukuranSepatu = 44;
+  double tinggiBadan = 170.0;
+  bool lebihDari = ukuranSepatu > tinggiBadan;
+  bool kurangDari = ukuranSepatu < tinggiBadan;
+  bool lebihDariSamaDengan = ukuranSepatu >= tinggiBadan;
+  bool kurangDariSamaDengan = ukuranSepatu <= tinggiBadan;
+  bool samaDengan = ukuranSepatu == tinggiBadan;
+  bool tidakSamaDengan = ukuranSepatu != tinggiBadan;
 
+  print("$ukuranSepatu > $tinggiBadan: $lebihDari");
+  print("$ukuranSepatu < $tinggiBadan: $kurangDari");
+  print("$ukuranSepatu >= $tinggiBadan: $lebihDariSamaDengan");
+  print("$ukuranSepatu <= $tinggiBadan: $kurangDariSamaDengan");
+  print("$ukuranSepatu == $tinggiBadan: $samaDengan");
+  print("$ukuranSepatu != $tinggiBadan: $tidakSamaDengan");
+}
---
+++ Day073.dart
@@ -0,0 +1,11 @@
+void main() {
+  int mahasiswa = 250;
+  int dosen = 200;
+
+  print("$mahasiswa > $dosen: ${mahasiswa > dosen}");
+  print("$mahasiswa < $dosen: ${mahasiswa < dosen}");
+  print("$mahasiswa >= $dosen: ${mahasiswa >= dosen}");
+  print("$mahasiswa <= $dosen: ${mahasiswa <= dosen}");
+  print("$mahasiswa == $dosen: ${mahasiswa == dosen}");
+  print("$mahasiswa != $dosen: ${mahasiswa != dosen}");
+}
