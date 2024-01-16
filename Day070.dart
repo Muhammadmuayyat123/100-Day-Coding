8fd2396811d4ba394968e200fe4610bf8fd4c11c
yayat <muhiyat9@gmail.com>
2024-01-16 12:51:23 +0700

    artimatika dart

---
+++ Day070.dart
@@ -0,0 +1,18 @@
+void main() {
+  int nilai1 = 15;
+  int nilai2 = 25;
+  int hasilPerjumlahan = nilai1 + nilai2;
+  print("hasil perjumlahan $nilai1 + $nilai2 = $hasilPerjumlahan");
+
+  int hasilPengurangan = nilai1 - nilai2;
+  print("hasil pengurangan $nilai1 - $nilai2 = $hasilPengurangan");
+
+  int hasilPerkalian = nilai1 * nilai2;
+  print("hasil perkalian $nilai1 x $nilai2 = $hasilPerkalian");
+
+  double hasilPembagian = nilai1 / nilai2;
+  print("hasil pembagian $nilai1 / $nilai2 = $hasilPembagian");
+
+  int hasilModulus = nilai1 % nilai2;
+  print("hasil Modulus $nilai1 % $nilai2 = $hasilModulus");
+}
---
+++ Day071.dart
@@ -0,0 +1,11 @@
+void main() {
+  int nilai1 = 20;
+  int nilai2 = 50;
+
+  print("operasi dasar");
+  print('Perjumlahan : $nilai1 + $nilai2 = ${nilai1 + nilai2}');
+  print('Pengurangan : $nilai1 - $nilai2 = ${nilai1 - nilai2}');
+  print('Perkalian : $nilai1 * $nilai2 = ${nilai1 * nilai2}');
+  print('Pembagian : $nilai1 / $nilai2 = ${nilai1 / nilai2}');
+  print('Modulus : $nilai1 % $nilai2 = ${nilai1 % nilai2}');
+}
